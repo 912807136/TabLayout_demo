d26ea284194397dd231dd4a3956470b62c1c64de
912807136 <912807136@qq.com>
2016-02-01 18:30:53 +0800

    Signed-off-by: 912807136 <912807136@qq.com>

--- Demo_TabLayout/src/com/gavin/demo_tablayout/MainActivity.java
+++ Demo_TabLayout/src/com/gavin/demo_tablayout/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends FragmentActivity {
 	private ViewPager viewPager;
@@ -17,35 +18,25 @@ public class MainActivity extends FragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		initView2();
+		initView();
 	}
 
-	private void initView2() {
+	private void initView() {
 		tabLayout = (TabLayout) findViewById(R.id.tablayout);
 		viewPager = (ViewPager) findViewById(R.id.pager);
-		viewPager.setOffscreenPageLimit(5);
+//		viewPager.setOffscreenPageLimit(5);
 		adapter = new TabPageIndicatorAdapter(getSupportFragmentManager());
 		adapter.setData(getCategories());
 		viewPager.setAdapter(adapter);
+		//设置TabLayout于ViewPager的关联
 		tabLayout.setupWithViewPager(viewPager);
-		
-//		tabLayout.setBackgroundColor(0xffeeeeee);
-//		tabLayout.setTabTextColors(Color.BLACK, Color.RED);// 设置文本在选中和为选中时候的颜色
-//		tabLayout.addTab(tabLayout.newTab().setText("第一个"), true);// 添加 Tab,默认选中
-//		tabLayout.addTab(tabLayout.newTab().setText("第二个"), false);// 添加Tab,默认不选中
-//		// 添加 Tab,默认不选中
-//		tabLayout.addTab(tabLayout.newTab().setText("第三个"), false);
-//		tabLayout.addTab(tabLayout.newTab().setText("第四个"), false);
 	}
 
 	private List<Category> getCategories() {
 		ArrayList<Category> list = new ArrayList<>();
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 8; i++) {
 			Category category = new Category();
-			category.setName("item "+i);
-			if (i==2) {
-				category.setName("item "+20000);
-			}
+			category.setName("选项 "+i);
 			category.setId(i);
 			list.add(category);
 		}
